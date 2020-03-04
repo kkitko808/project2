9a560c6e8f013d52bd5109d4d643527a32deb042
Stefania Stefansdottir <sstefansdottir88@gmail.com>
2020-03-04 08:26:56 -0600

    added inital routes

--- public/js/members.js
+++ public/js/members.js
@@ -5,5 +5,25 @@ $(document).ready(function () {
     $(".member-name").text(data.email);
   });
   // jqeury ajax requests to routes.
+  $.get("/api/trips", function(data) {
+
+    if (data.length !== 0) {
+  
+      for (var i = 0; i < data.length; i++) {
+  
+        var row = $("<div>");
+        row.addClass("trip");
+  
+        row.append("<p>" + data[i].destination + "</p>");
+        row.append("<p>" + data[i].activity + "</p>");
+        row.append("<p>" + data[i].time+ "</p>");
+  
+        $("#itinerary-area").prepend(row);
+  
+      }
+  
+    }
+  
+  });
 
 });
