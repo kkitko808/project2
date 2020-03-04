59e5567c2d0370c0dc042c70fb9c096847376037
Stefania Stefansdottir <sstefansdottir88@gmail.com>
2020-03-04 10:53:48 -0600

    Elements are appending and delete button is working

--- routes/trip-routes.js
+++ routes/trip-routes.js
@@ -5,7 +5,7 @@ var db = require("../models");
 module.exports = function (app) {
 
   // getting all trips
-  app.get("/api/trips", function (req, res) {
+  app.get("/api/members", function (req, res) {
     console.log(db);
     console.log(req);
     console.log(res);
@@ -18,13 +18,15 @@ module.exports = function (app) {
   // post
   app.post("/api/trips", function (req, res) {
     console.log(req.body);
+    console.log(res);
     db.Trips.create({
       destination: req.body.destination,
       activity: req.body.activity,
       time: req.body.time
     })
       .then(function (dbTrip) {
-        res.json(dbTrip);
+        // res.json(dbTrip);
+        console.log(dbTrip);
       });
   });
 
